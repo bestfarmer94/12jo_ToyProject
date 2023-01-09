eb29d19577a561bac9660f949455240448edb93a
SEOGUN <ksj083895@gmail.com>
2023-01-09 22:59:56 +0900

    Revert "Revert "feat : venv파일 생성, docs: app.py bs4 코드 추가""
    This reverts commit 4d39ec2d9f321e377b1d72b2e30428453153a1ff.

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from pymongo import MongoClient
 client = MongoClient("url")
 db = client.dbsparta
 
-#웹스크래핑 bs4 패키지 추가
-import requests
-from bs4 import BeautifulSoup
-
 #Flask 기본 코드
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
@@ -18,10 +14,6 @@ def home():
 @app.route('/user', methods=['POST'])
 def test_post():
    some_receive = request.form['some_give']
-
-   headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-   data = requests.get(some_receive,headers=headers)
-   soup = BeautifulSoup(data.text, 'html.parser')
 
    doc = {
     'some': some_receive
