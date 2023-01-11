aa5a04e43a953fa08488a74dc351ec49a3cecaf9
Seogun95 <ksj083895@gmail.com>
2023-01-11 15:05:50 +0900

    style: post 팝업 시트 추가, feat: save_bookmark.js 및 main.js 검색팝 기능 추가, docs: main 인덱스 포스팅 버튼 추가
    main.index에 GET으로 보여질 뼈대와 스타일 적용 부탁드립니다.

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,22 @@
-//==========================선중님
+/*==========포스팅 팝업*/
+$(document).ready(function () {
+    var target = $('#postPop');
+    $(document).on('click', '.nav__posting', function (e) {
+        target
+            .fadeIn(300, function () {
+                $('#postPop__url').focus();
+            })
+            .addClass('reveal');
+        $('body').addClass('has-url');
+    });
 
-//==========================성인님
+    $(document).mouseup(function (e) {
+        if (target.has(e.target).length == 0) {
+            target
+                .fadeOut(300, function () {
+                    $('body').removeClass('has-url');
+                })
+                .removeClass('reveal');
+        }
+    });
+});
