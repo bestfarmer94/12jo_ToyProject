97c20ff96169f52f597c3ab86a434f58ab70c757
Adult96 <djxor007@gmail.com>
2023-01-11 20:45:20 +0900

    feat: tagify라이브러리 기능 추가

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ db = client.dbsparta
 
 app = Flask(__name__)
 
-
+SECRET_KEY = "12jo"
 @app.route('/')
 def home():
    return render_template('index.html')
@@ -33,6 +33,8 @@ def login():
          
          if login['password'] == str(pw_receive):
             return jsonify({'complete': '로그인 완료'})
+         else:
+            return jsonify({'fail': '로그인 실패'})   
       else:
          login_list = list(db.login.find({'id':id_receive}, {'_id': False}))
          check_cnt = len(login_list)
@@ -49,7 +51,7 @@ def login():
 
          db.login.insert_one(doc)
       
-      return jsonify({'complete':'저장 완료'})
+      return jsonify({'create':'저장 완료'})
    else:
       return render_template('login.html')  
 
@@ -57,37 +59,67 @@ def login():
 def main():
    return render_template('main.html')
 
-# @app.route('/login', methods=['POST'])
-# def login_post():
-#    id_receive = request.form['id_give']
-#    pw_receive = request.form['pw_give']
-#    pw_check_receive = request.form['pw_check_give']
-#    # headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-#    # data = requests.get('',headers=headers)
-#    # soup = BeautifulSoup(data.text, 'html.parser')
 
-#    doc = {
-#    'id': id_receive,
-#    'password':generate_password_hash(password_receive)
-#    }
+@app.route("/save_bookmark", methods=["POST"])
+def save_bookmark():
+   # json -> dictionary
+   bookmark_data = json.loads(request.form["data_give"])
+   id_receive = bookmark_data["id"]
+   url_receive = bookmark_data["url"]
+   category_receive = bookmark_data["category"]
+   hash_receive = bookmark_data["hash"]
+   
+   all_list = list(db.bookmarks.find({}))
+   if len(all_list) != 0:
+      number = max(x["number"] for x in all_list) + 1
+   else:
+      number = 1
 
-#    print(doc)
-#    # db.login.insert_one(doc)
-#    print('get check', 'check')
-#    return jsonify({'msg':'저장 완료'})
+   bookmark_list = list(db.bookmarks.find({"id": id_receive, "url": url_receive}, {'_id': False}))
+   if len(bookmark_list) != 0:
+      return jsonify({"msg": "이미 가지고 있는 url 입니다."})
 
-# @app.route("/login", methods=["GET"])
-# def login_get():
-#    print('get check', 'check')
-#    login_list = list(db.login.find({}, {'_id': False}))
-#    print(login_list,'get check')
-#    return jsonify({'login_list': login_list})
+   headers = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+   data = requests.get(url_receive, headers=headers)
+   soup = BeautifulSoup(data.text, 'html.parser')
+
+   title = soup.select_one('meta[property="og:title"]')['content']
+   image = soup.select_one('meta[property="og:image"]')['content']
+
+   new_bookmark = {
+      "number": number,
+      "title": title,
+      "image": image,
+      "id": id_receive,
+      "url": url_receive,
+      "category": category_receive,
+      "hash": hash_receive
+   }
+   db.bookmarks.insert_one(new_bookmark)
+
+   category_list = list(db.categories.find({"id": id_receive, "category": category_receive}, {'_id': False}))
+
+   if len(category_list) == 0:
+      new_category = {
+            "category": category_receive,
+            "id": id_receive
+      }
+      db.categories.insert_one(new_category)
+
+   return jsonify({"msg": "저장 완료"})
 
 @app.route("/show_bookmark", methods=["POST"])
 def show_bookmark():
    id_receive = request.form["id_give"]
    bookmark_list = list(db.bookmarks.find({"id": id_receive}, {'_id': False}))
    return jsonify({'bookmark_list': bookmark_list})    
+
+@app.route("/delete", methods=["POST"])
+def delete_bookmark():
+   number_receive = request.form["number_give"]
+   db.bookmarks.delete_one({"number": number_receive})
+   return jsonify({"msg": "삭제 완료"})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
